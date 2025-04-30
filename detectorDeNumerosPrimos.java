28c8d47ea756d378b11cb19778b6e62047635803
GustavoGraciano08 <ggraciano.sesi08@gmail.com>
2025-04-30 14:00:55 -0300

    detectorDeNumerosPrimos.java

--- detectorDeNumerosPrimos.java
+++ detectorDeNumerosPrimos.java
@@ -1,5 +1,35 @@
 package desafiosRevisao;
 
+import java.util.Scanner;
+
 public class detectorDeNumerosPrimos {
+	public static void main(String[] args) {
+		
+		 Scanner ler = new Scanner(System.in);
+
+	        System.out.print("Digite um número inteiro positivo: ");
+	        int numero = ler.nextInt();
+
+	        if (numero <= 1) {
+	            System.out.println(numero + " não é um número primo.");
+	        } else {
+	            int divisorEncontrado = 0;
+
+	            for (int i = 2; i * i <= numero; i++) {
+	                if (numero % i == 0) {
+	                    divisorEncontrado = 1;
+	                    break;
+	                }
+	            }
+
+	            if (divisorEncontrado == 0) {
+	                System.out.println(numero + " é um número primo.");
+	            } else {
+	                System.out.println(numero + " não é um número primo.");
+	            }
+	        }
+
+	        ler.close();
+	}
 
 }
